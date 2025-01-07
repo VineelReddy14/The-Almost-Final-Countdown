691eb5280a5ad77321bf113a0e8d0ee8b7ca6090
Vineel Palla <vineelpalla14@gmail.com>
2025-01-07 14:40:11 -0800

    Set UserName using useState and useRef

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const palyerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnteredPlayerName(palyerName.current.value);
+    palyerName.current.value = '';
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <p>
+        <input 
+          ref={palyerName} 
+          type="text"
+        />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
